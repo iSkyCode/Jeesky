3c95ca692f87ce2f8c34a55b5ee93889555c3aa1
yanjunfeng <yanjunfeng2011@live.cn>
2017-08-16 17:30:04 +0800

    优化：调整结构。

--- src/main/java/org/iskycode/jeesky/web/UserController.java
+++ src/main/java/org/iskycode/jeesky/web/UserController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/user")
 @Controller
 public class UserController {
-	@ResponseBody
-	@RequestMapping(value = "/login")
-	public String login() {
-		return "index";
-	}
+
+    @RequestMapping(value = "/login")
+    public String login() {
+        return "index";
+    }
 }
